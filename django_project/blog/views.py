06f8bc930693b198713102fbb4cae78067b69305
metopotato <14121611ll@gmail.com>
2021-05-25 10:11:00 +0700

    add gitignore

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Linh',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted':'August 2,2020'
-    },
-        {
-        'author': 'James',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted':'August 3,2020'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    context = {
+        'title':'about'
+    }
+    return render(request,'blog/about.html',context)
