3a60b7bd0361c468c5062d3795e2850c854974cc
metopotato <14121611ll@gmail.com>
2021-05-26 09:08:04 +0700

    commits

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -16,3 +16,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
